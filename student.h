1e77aac986ef48af0f2951d2e5481db523b5a0f0
Alexcyfy <acollanton@mail.csuchico.edu>
2018-12-03 16:05:06 -0800

    Implemented member varaibles and function prototype

---
+++ student.h
@@ -0,0 +1,14 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+#endif
+
+class student
+{
+private:
+std::string firstName;
+std::string lastName;
+
+public:
+std::string setName(std::string, std::string);
+
+};
